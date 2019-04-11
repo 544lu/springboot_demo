19e03ab97d5ef47a9ed380b5ed3b1043c88560a0
Administrator <Administrator@PC-201901101141>
2019-04-11 12:40:46 +0800

    first commit,2019.4.11

---
+++ mySpringboot_project/demo_1/src/main/java/com/lu/demo_1/Controller_test.java
@@ -0,0 +1,55 @@
+package com.lu.demo_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.demo_1.entity.LearnResouce;
+
+@Controller
+public class Controller_test {
+	@Autowired
+	ConfigBean configBean;
+	
+    @RequestMapping(value="first")
+    @ResponseBody
+    public String first() {   	
+    	return "hello springboot"+configBean.getName()+configBean.getAge();
+    }
+    
+    @RequestMapping(value="index")
+    public ModelAndView index(){
+    	System.out.println("调用index方法");
+        List<LearnResouce> learnList =new ArrayList<LearnResouce>();
+        LearnResouce bean =new LearnResouce("官方参考文档","Spring Boot Reference Guide","http://docs.spring.io/spring-boot/docs/1.5.1.RELEASE/reference/htmlsingle/#getting-started-first-application");
+        learnList.add(bean);
+        bean =new LearnResouce("官方SpriongBoot例子","官方SpriongBoot例子","https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples");
+        learnList.add(bean);
+        bean =new LearnResouce("龙国学院","Spring Boot 教程系列学习","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResouce("嘟嘟MD独立博客","Spring Boot干货系列 ","http://tengj.top/");
+        learnList.add(bean);
+        bean =new LearnResouce("后端编程嘟","Spring Boot教程和视频 ","http://www.toutiao.com/m1559096720023553/");
+        learnList.add(bean);
+        bean =new LearnResouce("程序猿DD","Spring Boot系列","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResouce("纯洁的微笑","Sping Boot系列文章","http://www.ityouknow.com/spring-boot");
+        learnList.add(bean);
+        bean =new LearnResouce("CSDN——小当博客专栏","Sping Boot学习","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResouce("梁桂钊的博客","Spring Boot 揭秘与实战","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResouce("林祥纤博客系列","从零开始学Spring Boot ","http://412887952-qq-com.iteye.com/category/356333");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index1");
+        modelAndView.addObject("learnList", learnList);
+        return modelAndView;
+    }
+}
---
+++ mySpringboot_project/demo_1/src/main/java/com/lu/demo_1/Demo1Application.java
@@ -0,0 +1,37 @@
+package com.lu.demo_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableConfigurationProperties({ConfigBean.class})
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+	
+	@Bean(destroyMethod =  "close")
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test1");
+        dataSource.setUsername("root");//用户名
+        dataSource.setPassword("root");//密码
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setInitialSize(2);//初始化时建立物理连接的个数
+        dataSource.setMaxActive(20);//最大连接池数量
+        dataSource.setMinIdle(0);//最小连接池数量
+        dataSource.setMaxWait(60000);//获取连接时最大等待时间，单位毫秒。
+        dataSource.setValidationQuery("SELECT 1");//用来检测连接是否有效的sql
+        dataSource.setTestOnBorrow(false);//申请连接时执行validationQuery检测连接是否有效
+        dataSource.setTestWhileIdle(true);//建议配置为true，不影响性能，并且保证安全性。
+        dataSource.setPoolPreparedStatements(false);//是否缓存preparedStatement，也就是PSCache
+        return dataSource;
+    }
+
+}
